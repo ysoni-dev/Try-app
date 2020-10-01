920ee751e8023ef20a21e29afbd993ad946a9aad
ysoni-dev <ysoni1614@gmail.com>
2020-10-01 19:19:04 +0530

    Updates

---
+++ precache-manifest.3e366a82a685feb9e4e4ecb06b688f38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb2b64debc38f9886da222c59b2cb52",
+    "url": "/Try-app/index.html"
+  },
+  {
+    "revision": "577283043cec4030fcb9",
+    "url": "/Try-app/static/css/main.bc38e607.chunk.css"
+  },
+  {
+    "revision": "184f83c7b1474be8ebea",
+    "url": "/Try-app/static/js/2.76b20dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Try-app/static/js/2.76b20dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577283043cec4030fcb9",
+    "url": "/Try-app/static/js/main.74970bbd.chunk.js"
+  },
+  {
+    "revision": "f10c8cb63537aaf515e8",
+    "url": "/Try-app/static/js/runtime-main.832694c3.js"
+  },
+  {
+    "revision": "db21e780d7879c2e25c113fa63ad90c4",
+    "url": "/Try-app/static/media/furniture.db21e780.mp4"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Try-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
